582fd3089099515feed0dbeba3acbb6dcc74bffa
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-06-14 21:18:01 +0200

    create react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./home/Home";
+import About from "./about/About";
+import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 
 const App = () => {
   return (
@@ -7,7 +12,14 @@ const App = () => {
         <div className="app-header-inner-wrapper"></div>
       </header>
       <main className="app-main">
-        <div className="app-main-inner-wrapper"></div>
+        <div className="app-main-inner-wrapper">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
       </main>
       <footer className="app-footer">
         <div className="app-footer-inner-wrapper"></div>
